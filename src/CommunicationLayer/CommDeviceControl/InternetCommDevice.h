f21edb641dfa77e398ce7c886d0b25ed260253a6
Muhammad Muzammil <66068301+muzman123@users.noreply.github.com>
2024-03-18 18:25:40 -0600

    Update InternetCommDevice.h

--- src/CommunicationLayer/CommDeviceControl/InternetCommDevice.h
+++ src/CommunicationLayer/CommDeviceControl/InternetCommDevice.h
@@ -2,7 +2,6 @@
 
 class QIODevice;
 
-// #include <SimpleAmqpClient/SimpleAmqpClient.h>
 #include <QByteArray>
 #include <QThread>
 #include <QMqttClient>
@@ -16,12 +15,10 @@ public:
     void setQueueName(QString queueName);
     void connectToBroker();
     void subscribeToTopic(QString topic);
-    //void setChannel(AmqpClient::Channel::ptr_t channel);
     virtual void run() override;
     QMqttClient *getClient();
-private:
-    //AmqpClient::Channel::ptr_t channel_;
 
+private:
     QString queueName_;
     QMqttClient* mqttClient;
     QString ipAddress_;
